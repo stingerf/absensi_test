46ce631a32b480a261c47dc413b9814093b4e82f
rafif <rafifidnsolo007@gmail.com>
2025-02-19 08:34:12 +0700

    main dart placeholder

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,64 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          // Add your own Firebase project configuration from google-services.json
+          apiKey: 'AIzaSyDAkwcmlF9H4Tos_J1fW7JbmAXGFGJsub8', // api_key
+          appId:
+              '1:554782741519:android:253e24075f271c04d37f7a', // mobilesdk_app_id
+          messagingSenderId: '554782741519', // project_number
+          projectId: 'absensi-test-app' // project_id
+          ),
+    );
+    // Firebase connection success
+    print("Firebase Terhubung ke:");
+    print("API Key: ${Firebase.app().options.apiKey}");
+    print("Project ID: ${Firebase.app().options.projectId}");
+  } catch (e) {
+    // Firebase connection failed
+    print("Firebase gagal terhubung: $e");
+  }
+  // runApp(const HomeScreen());
+  runApp(const TestApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class TestApp extends StatelessWidget {
+  // Main App
+  const TestApp({super.key}); // Constructor of TestApp clas
+
+  @override // can give information about about your missing override code
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // remove debug banner
+      home: const HomeScreen(), // HomeScreen class
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 50, // Header
+            child: Placeholder(),
+          ),
+          Expanded(
+            child: Placeholder(), // Content
+          ),
+          SizedBox(
+            height: 50, // Footer
+            child: Placeholder(),
+          ),
+        ],
       ),
     );
   }
