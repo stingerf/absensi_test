180cc0083b01dfa184135132503273a398c1bea6
rafif <rafifidnsolo007@gmail.com>
2025-02-26 08:07:16 +0700

    attend sceen

--- lib/ui/attend/attend_screen.dart
+++ lib/ui/attend/attend_screen.dart
@@ -1,44 +1,499 @@
+import 'dart:io'; // Provides access to file and directory operations for working with the filesystem.
+import 'package:absensiapp/ui/attend/camera_sccreen.dart';
+import 'package:absensiapp/ui/home_screen.dart';
+import 'package:camera/camera.dart'; // Allows access to the device camera for capturing photos or videos.
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dotted_border/dotted_border.dart'; // Provides a widget to create a dotted or dashed border around elements.
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart'; // Converts coordinates into addresses.
+import 'package:geolocator/geolocator.dart'; // Handles retrieving the device’s current location and managing GPS permissions.
+import 'package:intl/intl.dart'; // Used for formatting dates, numbers, and localization support.
 
-class AttendScreen extends StatelessWidget {
-  const AttendScreen({super.key});
+
+class AttendScreen extends StatefulWidget {
+  final XFile? image;
+
+  const AttendScreen({super.key, this.image});
+
+  @override
+  State<AttendScreen> createState() => _AttendScreenState(image);
+}
+
+class _AttendScreenState extends State<AttendScreen> {
+  _AttendScreenState(this.image);
+
+  XFile? image;
+  String strAlamat = "",
+      strDate = "",
+      strTime = "",
+      strDateTime = "",
+      strStatus = "Attend";
+  bool isLoading = false;
+  double dLat = 0.0, dLong = 0.0;
+  int dateHours = 0, dateMinutes = 0;
+  final controllerName = TextEditingController();
+  final CollectionReference dataCollection =
+      FirebaseFirestore.instance.collection('attendance');
+
+  @override
+  void initState() {
+    handleLocationPermission();
+    setDateTime();
+    setStatusAbsen();
+
+    if (image != null) {
+      isLoading = true;
+      getGeoLocationPosition();
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          // Header
-          SizedBox(
-            height: 50,
-            child: Scaffold(
-              appBar: AppBar(
-                backgroundColor: Colors.blueAccent,
-                iconTheme: const IconThemeData(color: Colors.white),
-                centerTitle: true,
-                title: const Text(
-                  "Attendance - Flutter App Admin",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-              ),
-            ),
-          ),
+    Size size = MediaQuery.of(context).size;
 
-          // Content
-          Expanded(
-            child: Column(
-              children: [
-                Expanded(
-                  child: Text('Attend Screen View'),
-                ),
-              ],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: const Color.fromARGB(255, 26, 0, 143),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        centerTitle: true,
+        title: const Text(
+          "Attendance Menu",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Card(
+            color: Colors.white,
+            margin: const EdgeInsets.fromLTRB(10, 10, 10, 30),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
             ),
+            elevation: 5,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 50,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10)),
+                    color: Colors.blueAccent,
+                  ),
+                  child: const Row(
+                    children: [
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Icon(Icons.face_retouching_natural_outlined,
+                          color: Colors.white),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Text(
+                        "Please make a selfie photo!",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(10, 20, 0, 20),
+                  child: Text(
+                    "Capture Photo",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const CameraScreen()));
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.fromLTRB(10, 0, 10, 20),
+                    width: size.width,
+                    height: 150,
+                    child: DottedBorder(
+                      radius: const Radius.circular(10),
+                      borderType: BorderType.RRect,
+                      color: Colors.blueAccent,
+                      strokeWidth: 1,
+                      dashPattern: const [5, 5],
+                      child: SizedBox.expand(
+                        child: FittedBox(
+                          child: image != null
+                              ? Image.file(File(image!.path), fit: BoxFit.cover)
+                              : const Icon(
+                                  Icons.camera_enhance_outlined,
+                                  color: Colors.blueAccent,
+                                ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.text,
+                    controller: controllerName,
+                    decoration: InputDecoration(
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 10),
+                      labelText: "Your Name",
+                      hintText: "Please enter your name",
+                      hintStyle:
+                          const TextStyle(fontSize: 14, color: Colors.grey),
+                      labelStyle:
+                          const TextStyle(fontSize: 14, color: Colors.black),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.blueAccent),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.blueAccent),
+                      ),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  child: Text(
+                    "Your Location",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                ),
+                isLoading
+                    ? const Center(
+                        child:
+                            CircularProgressIndicator(color: Colors.blueAccent))
+                    : Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: SizedBox(
+                          height: 5 * 24,
+                          child: TextField(
+                            enabled: false,
+                            maxLines: 5,
+                            decoration: InputDecoration(
+                              alignLabelWithHint: true,
+                              disabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide:
+                                    const BorderSide(color: Colors.blueAccent),
+                              ),
+                              hintText: strAlamat,
+                              hintStyle: const TextStyle(
+                                  fontSize: 14, color: Colors.grey),
+                              fillColor: Colors.transparent,
+                              filled: true,
+                            ),
+                          ),
+                        ),
+                      ),
+                Container(
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.all(30),
+                    child: Material(
+                      elevation: 3,
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        width: size.width,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white),
+                        child: Material(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.blueAccent,
+                          child: InkWell(
+                            splashColor: Colors.blue,
+                            borderRadius: BorderRadius.circular(20),
+                            onTap: () {
+                              if (image == null ||
+                                  controllerName.text.isEmpty) {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(const SnackBar(
+                                  content: Row(
+                                    children: [
+                                      Icon(
+                                        Icons.info_outline,
+                                        color: Colors.white,
+                                      ),
+                                      SizedBox(width: 10),
+                                      Text("Please Fill all the forms!",
+                                          style: TextStyle(color: Colors.white))
+                                    ],
+                                  ),
+                                  backgroundColor: Colors.blueGrey,
+                                  shape: StadiumBorder(),
+                                  behavior: SnackBarBehavior.floating,
+                                ));
+                              } else {
+                                submitAbsen(strAlamat,
+                                    controllerName.text.toString(), strStatus);
+                              }
+                            },
+                            child: const Center(
+                              child: Text(
+                                "Report Now",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
+              ],
+            )),
+      ),
+    );
+  }
+
+  //get realtime location
+  Future<void> getGeoLocationPosition() async {
+    // ignore: deprecated_member_use
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    setState(() {
+      isLoading = false;
+      getAddressFromLongLat(position);
+    });
+  }
+
+  //get address by lat long
+  Future<void> getAddressFromLongLat(Position position) async {
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    print(placemarks);
+    Placemark place = placemarks[0];
+    setState(() {
+      dLat = double.parse('${position.latitude}');
+      dLat = double.parse('${position.longitude}');
+      strAlamat =
+          "${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}";
+    });
+  }
+
+  //permission location
+  Future<bool> handleLocationPermission() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Row(
+          children: [
+            Icon(
+              Icons.location_off,
+              color: Colors.white,
+            ),
+            SizedBox(width: 10),
+            Text(
+              "Location services are disabled. Please enable the services.",
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+        backgroundColor: Colors.blueGrey,
+        shape: StadiumBorder(),
+        behavior: SnackBarBehavior.floating,
+      ));
+      return false;
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Row(
+            children: [
+              Icon(
+                Icons.location_off,
+                color: Colors.white,
+              ),
+              SizedBox(width: 10),
+              Text(
+                "Location permission denied.",
+                style: TextStyle(color: Colors.white),
+              )
+            ],
+          ),
+          backgroundColor: Colors.blueGrey,
+          shape: StadiumBorder(),
+          behavior: SnackBarBehavior.floating,
+        ));
+        return false;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Row(
+          children: [
+            Icon(
+              Icons.location_off,
+              color: Colors.white,
+            ),
+            SizedBox(width: 10),
+            Text(
+              "Location permission denied forever, we cannot access.",
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+        backgroundColor: Colors.blueGrey,
+        shape: StadiumBorder(),
+        behavior: SnackBarBehavior.floating,
+      ));
+      return false;
+    }
+    return true;
+  }
+
+  //show progress dialog
+  showLoaderDialog(BuildContext context) {
+    AlertDialog alert = AlertDialog(
+      content: Row(
+        children: [
+          const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.blueGrey)),
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: const Text("Checking the data..."),
           ),
         ],
       ),
     );
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  //check format date time
+  void setDateTime() async {
+    var dateNow = DateTime.now();
+    var dateFormat = DateFormat('dd MMMM yyyy');
+    var dateTime = DateFormat('HH:mm:ss');
+    var dateHour = DateFormat('HH');
+    var dateMinute = DateFormat('mm');
+
+    setState(() {
+      strDate = dateFormat.format(dateNow);
+      strTime = dateTime.format(dateNow);
+      strDateTime = "$strDate | $strTime";
+
+      dateHours = int.parse(dateHour.format(dateNow));
+      dateMinutes = int.parse(dateMinute.format(dateNow));
+    });
+  }
+
+  //check status absent
+  void setStatusAbsen() {
+    if (dateHours < 8 || (dateHours == 8 && dateMinutes <= 30)) {
+      strStatus = "Attend";
+    } else if ((dateHours > 8 && dateHours < 18) ||
+        (dateHours == 8 && dateMinutes >= 31)) {
+      strStatus = "Late";
+    } else {
+      strStatus = "Leave";
+    }
+  }
+
+  //submit data absent to firebase
+  Future<void> submitAbsen(String alamat, String nama, String status) async {
+    showLoaderDialog(context);
+    dataCollection.add({
+      'address': alamat,
+      'name': nama,
+      'description': status,
+      'datetime': strDateTime
+    }).then((result) {
+      setState(() {
+        Navigator.of(context).pop();
+        try {
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Row(
+              children: [
+                Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 10),
+                Text("Yeay! Attendance Report Succeeded!",
+                    style: TextStyle(color: Colors.white))
+              ],
+            ),
+            backgroundColor: Colors.orangeAccent,
+            shape: StadiumBorder(),
+            behavior: SnackBarBehavior.floating,
+          ));
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => const HomeScreen()));
+        } catch (e) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Row(
+              children: [
+                const Icon(
+                  Icons.info_outline,
+                  color: Colors.white,
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Text("Ups, $e",
+                      style: const TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+            backgroundColor: Colors.blueGrey,
+            shape: const StadiumBorder(),
+            behavior: SnackBarBehavior.floating,
+          ));
+        }
+      });
+    }).catchError((error) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Row(
+          children: [
+            const Icon(
+              Icons.error_outline,
+              color: Colors.white,
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+                child: Text("Ups, $error",
+                    style: const TextStyle(color: Colors.white)))
+          ],
+        ),
+        backgroundColor: Colors.blueGrey,
+        shape: const StadiumBorder(),
+        behavior: SnackBarBehavior.floating,
+      ));
+      Navigator.of(context).pop();
+    });
   }
 }
---
+++ lib/utils/face_detection/google_ml_kit.dart
@@ -0,0 +1,8 @@
+import 'package:absensiapp/utils/face_detection/vision.dart';
+
+// Face recognition using Google ML Kit
+class GoogleMlKit {
+  GoogleMlKit._();
+
+  static final Vision vision = Vision.instance;
+}
